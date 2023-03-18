d11b412d09a1e81eae24c46ebd18040f60031775
Administrator <318384701@qq.com>
2023-03-18 16:47:58 +0800

    miaosha

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.controller.viewobject.UserVO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller("user")
 @RequestMapping("/user")
@@ -18,16 +24,39 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserVO getUser(@RequestParam(name = "id") Integer id){
-        UserModel userModel=userService.getUserById(id);
-        return convertFromModel(userModel);
+    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) throws BusinessException {
+        UserModel userModel = userService.getUserById(id);
+
+        if (userModel == null) {
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
+        UserVO userVO = convertFromModel(userModel);
+        return CommonReturnType.create(userVO);
     }
-    private UserVO convertFromModel(UserModel userModel){
-        if (userModel==null){
+
+    private UserVO convertFromModel(UserModel userModel) {
+        if (userModel == null) {
             return null;
         }
-        UserVO userVO=new UserVO();
-        BeanUtils.copyProperties(userModel,userVO);
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel, userVO);
         return userVO;
     }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Object handlerException(HttpServletRequest request, Exception ex) {
+        BusinessException businessException = (BusinessException) ex;
+        CommonReturnType commonReturnType = new CommonReturnType();
+        commonReturnType.setStatus("fail");
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("errCode", businessException.getErrCode());
+        responseData.put("errMsg", businessException.getErrMsg());
+        commonReturnType.setData(responseData);
+        return commonReturnType;
+    }
+
+
 }
